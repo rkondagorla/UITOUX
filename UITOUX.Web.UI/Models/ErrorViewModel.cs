b829d77d3289f2526314fef8252b0bd8f26f792b
Ramakrishna <ramakrishna.kondagorla@betalen.in>
2024-05-08 12:17:21 +0530

    commit

---
+++ UITOUX.Web.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UITOUX.Web.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
