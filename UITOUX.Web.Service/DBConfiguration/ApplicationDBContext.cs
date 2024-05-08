40a27a5dec8bc5cde323da6e609d0e01fd5137e3
Ramakrishna <ramakrishna.kondagorla@betalen.in>
2024-05-08 12:53:18 +0530

    Sql Lite Local/In Memory Database Configuration

---
+++ UITOUX.Web.Service/DBConfiguration/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UITOUX.Web.Service.DBConfiguration
+{
+    public class ApplicationDBContext  : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ UITOUX.Web.Service/DBConfiguration/SeedData.cs
@@ -0,0 +1,10 @@
+﻿namespace UITOUX.Web.Service.DBConfiguration
+{
+    public static class SeedData
+    {
+        public static void Initialize(ApplicationDBContext context)
+        {
+            context.SaveChanges();
+        }
+    }
+}
