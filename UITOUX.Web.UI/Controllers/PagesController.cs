ab6c7cad86fd06b5c1b259ea079eaac4c17fc923
Ramakrishna <ramakrishna.kondagorla@betalen.in>
2024-05-09 11:46:51 +0530

    adding footer infor

---
+++ UITOUX.Web.UI/Controllers/PagesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UITOUX.Web.UI.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult InvestorRelations()
+        {
+            return View();
+        }
+        public IActionResult SellersRelations()
+        {
+            return View();
+        }
+        public IActionResult ReturnPolicy()
+        {
+            return View();
+        }
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+        public IActionResult Disclaimer()
+        {
+            return View();
+        }
+        public IActionResult TermsOfUse()
+        {
+            return View();
+        }
+        public IActionResult BuyerTermsAndConditions()
+        {
+            return View();
+        }
+        public IActionResult SellerTermsAndConditions()
+        {
+            return View();
+        }
+    }
+}
